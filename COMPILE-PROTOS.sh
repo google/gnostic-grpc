99785d2c6664cc32ce523ba43073812d9a8c563f
raul palomo <raulpalomo@google.com>
2021-06-29 15:20:28 -0400

    getting protoc-gen-go from protobuf

--- COMPILE-PROTOS.sh
+++ COMPILE-PROTOS.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 go get github.com/golang/protobuf@v1.4.2
+go install github.com/golang/protobuf/protoc-gen-go
 protoc --go_out=incompatibility/incompatibility-report/ incompatibility/incompatibility-report/incompatibility-report.proto
