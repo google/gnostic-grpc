fa076e711668a46da34353f151ea7d886f4f89c5
raulpalomo <raulpalomo8@gmail.com>
2021-07-29 15:58:03 -0400

    CI fix

--- COMPILE-PROTOS.sh
+++ COMPILE-PROTOS.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-go install github.com/golang/protobuf/protoc-gen-go
+# go install github.com/golang/protobuf/protoc-gen-go
 protoc --go_out=incompatibility/ incompatibility/incompatibility-report.proto
-go get github.com/golang/protobuf@v1.4.2
-go get
+# go get github.com/golang/protobuf@v1.4.2
+go get ./...
--- plugin-creation.sh
+++ plugin-creation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gopath=$(go env GOPATH)
-./COMPILE-PROTOS.sh
-go build -o ${gopath}/bin/gnostic-grpc
+# ./COMPILE-PROTOS.sh
+go build -o ${gopath}/bin/gnostic-grpc plugin
