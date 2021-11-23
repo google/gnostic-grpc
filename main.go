ed44b3114769a9162c671aeb909dc1fb39c8041b
Karim Benhammou <karim.benhammou@42crunch.com>
2021-11-23 15:24:00 +0100

    turn into package

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/karim42benhammou/gnostic-grpc/plugin"
+
+func main() {
+	plugin.Main()
+}
--- plugin/main.go
+++ plugin/main.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package plugin
 
 import (
 	"errors"
@@ -23,7 +23,7 @@ import (
 	"github.com/karim42benhammou/gnostic-grpc/incompatibility"
 )
 
-func main() {
+func Main() {
 	env, err := plugins.NewEnvironment()
 	env.RespondAndExitIfError(err)
 	switch paramLen := len(env.Request.Parameters); paramLen {
